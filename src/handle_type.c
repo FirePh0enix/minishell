fe091b37d499fbe7c11bfbce1beb56803381da0b
FirePh0enix <leonard@delbecq.net>
2024-04-17 00:47:29 +0200

    Fix a lot a stuff, but also stuff broke, like new prompt on a few commands

--- src/handle_type.c
+++ src/handle_type.c
@@ -6,7 +6,7 @@
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 15:29:38 by vopekdas          #+#    #+#             */
-/*   Updated: 2024/04/15 16:06:47 by ledelbec         ###   ########.fr       */
+/*   Updated: 2024/04/17 00:38:00 by ledelbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ int	handle_pipe(t_minishell *msh, t_node *node, int in, int out)
 	close(fd[1]);
 	status = exec_cmd(msh, node->pipe.right, fd[0], out);
 	close(fd[0]);
+	status = wait_for_children(msh);
 	return (status);
 }
 
@@ -42,11 +43,9 @@ int	handle_or(t_minishell *msh, t_node *node, int in, int out)
 	int	status;
 
 	status = exec_cmd(msh, node->pipe.left, in, out);
-	status = wait_for_children(msh);
 	if (status == 0)
 		return (status);
 	status = exec_cmd(msh, node->pipe.right, in, out);
-	status = wait_for_children(msh);
 	return (status);
 }
 
@@ -55,11 +54,9 @@ int	handle_and(t_minishell *msh, t_node *node, int in, int out)
 	int	status;
 
 	status = exec_cmd(msh, node->pipe.left, in, out);
-	status = wait_for_children(msh);
 	if (status != 0)
 		return (status);
 	status = exec_cmd(msh, node->pipe.right, in, out);
-	status = wait_for_children(msh);
 	return (status);
 }
 
@@ -71,9 +68,7 @@ int	handle_parent(t_minishell *msh, t_node *node, int in, int out)
 	if (pid == -1)
 		return (-1);
 	if (pid == 0)
-	{
 		exit(exec_cmd(msh, node->parent.node, in, out));
-	}
 	return (wait_for_children(msh));
 }
 
